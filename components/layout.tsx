6fc64eff677d35a8fb21d117e47cfc8e7b9b3cc4
lawliet <lawlietcos@outlook.com>
2022-02-23 23:44:24 +0800

    Add: init blog setting

---
+++ components/layout.tsx
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const name = "lawliet";
+export const siteTitle = "lawliet's Note";
+
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="lawliet, cos, blog." />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className="h-24 shadow">
+        <div className="max-w-screen-xl w-11/12 h-full flex flex-row justify-between mx-auto items-center">
+          <Link href="/">
+            <a className="text-4xl">LAWLIET</a>
+          </Link>
+          <Link href="/about">
+            <a>
+              <Image
+                priority
+                src="/images/profile.jpg"
+                height={48}
+                width={48}
+                alt={name}
+              />
+            </a>
+          </Link>
+        </div>
+      </header>
+      <main className="flex-1 max-w-screen-xl w-11/12 mx-auto">
+        {children}
+        {!home && (
+          <div className="my-4">
+            <Link href="/">
+              <a className="text-green-500">← Back to home</a>
+            </Link>
+          </div>
+        )}
+      </main>
+      <footer className="leading-10 text-gray-400 text-center border-t-2 border-gray-100">
+        © I can fly. - lawliet
+      </footer>
+    </div>
+  );
+}
---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Layout from "../components/layout"
+
+const name = "lawliet";
+export const siteTitle = "lawliet's Note";
+
+export default function About() {
+  return (
+    <Layout home>
+      <Head>
+        <title>About lawliet</title>
+      </Head>
+      !!!!!!!
+    </Layout>
+  );
+}
